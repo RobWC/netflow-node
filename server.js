91169f6c9041388c352a59a6022eed83f8fe1e39
Rob Cameron <rwcameron@gmail.com>
2012-03-27 22:14:39 -0700

    changing flowset decode

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ var server = dgram.createSocket("udp4");
 
 server.on("message", function (msg, rinfo) {
   console.log("ver " + msg.readUInt16BE(0));
-  console.log("count " + msg.readUInt16BE(2));
-  console.log("sysUpTime " + msg.readUInt32BE(4));
-  console.log("UnixSec " + msg.readUInt32BE(8));
-  console.log("seq " + msg.readUInt32BE(12));
-  console.log("sourceID " + msg.readUInt32BE(16));
+  console.log("count " + msg.readUInt16BE(2)); //2
+  console.log("sysUpTime " + msg.readUInt32BE(4)); //6
+  console.log("UnixSec " + msg.readUInt32BE(8)); //12
+  console.log("seq " + msg.readUInt32BE(12)); //24
+  console.log("sourceID " + msg.readUInt32BE(16)); //40
   
-  //loop through the flow sets 59 bytes each
-  var flowset = new Buffer(59);
-  msg.copy(flowset,0,20,79);
+  //pull the flowset header to check its length
+  var flowset = new Buffer(4);
+  msg.copy(flowset,0,20,24);
   
   console.log("FSID " + flowset.readUInt16BE(0));
   console.log("FS LEN " + flowset.readUInt16BE(2));
-  console.log("FS octets " + parseInt(flowset.readDoubleBE(4)));
+  //console.log("FS octets " + parseInt(flowset.readDoubleBE(4)));
   console.log("");
 
 
-  //console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
 });
 
 server.on("listening", function () {
